e13f435a8f8e83e5ed773b67c20ca0fa9f856e68
Joshan Rai <pradheon@gmail.com>
2022-04-13 15:29:48 -0700

    Initial Commit

---
+++ JoshanRai-BucketList/JoshanRai_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_BucketListApp.swift
+//  JoshanRai-BucketList
+//
+//  Created by Joshan Rai on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
